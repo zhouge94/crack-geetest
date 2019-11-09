b61ee04c70892241ef1d29ef9090f9e94036a279
zhouge@home <you@example.com>
2019-11-10 00:00:30 +0800

    ok

--- test.js
+++ test.js
@@ -5,37 +5,30 @@ let request = require('superagent');
 
 async function testOnce() {
     // B站登录
+
     let site = 'https://040552.com/';
-    let agent=request.agent()
-    r=await agent.get(site)
-    console.log(r.text)
+    /*    r=await request.get(site)
     var pat=/<iframe id=\"main-iframe\" src=\"(.*?)\" frameborder/g
     referer=pat.exec(r.text)[1]
     console.log(referer)
-    cookies=r.header["set-cookie"]
     let cookies_str=""
-    for(i in cookies){
-        cookies_str+=String(cookies[i]).split(";")[0]+";"
+    for(i in r.header["set-cookie"]){
+        cookies_str+=String(r.header["set-cookie"][i]).split(";")[0]+";"
     }
+    console.log(cookies_str)
+    r=await request.get(site+'/_Incapsula_Resource?SWCNGEEC=1').set("cookie",cookies_str)
 
-    console.log(cookies,cookies_str)
-    r=await agent.get(site+'/_Incapsula_Resource').query({SWCNGEEC:1})
     text=r.text
+    console.log(text)
     let config = JSON.parse(text);
     should(config.gt).be.a.String().and.not.empty();
     should(config.challenge).be.a.String().and.not.empty();
+    */
+    config=
+    {"challenge": "aca5a28e3e72dc9d2655154b8c1a4809", "gt": "98443ddedafab16e689467f796c2c4d6", "new_captcha": true, "success": 1}
     let gt = config.gt;
     let challenge = config.challenge;
-
-    // B站注册
-    // let config = JSON.parse((await request.get('https://passport.bilibili.com/web/captcha/combine').query({
-    //     plat:6
-    // })).text).data.result;
-    // should(config.gt).be.a.String().and.not.empty();
-    // should(config.challenge).be.a.String().and.not.empty();
-    // let gt = config.gt;
-    // let challenge = config.challenge;
-    // let site = 'https://passport.bilibili.com/register/phone.html';
+    console.log(config)
 
     let crack = await crack_geetest(gt, challenge, site, { debug: false });
     console.log(crack);
@@ -45,24 +38,18 @@ async function testOnce() {
     var post_body = "geetest_challenge=" + crack.challenge 
     + "&geetest_validate=" +  crack.validate 
     + "&geetest_seccode=" + crack.validate+"|jordan";
-    console.log(r.header["cookies"])
-
+    console.log(post_body)
+/*
     r=await request.post(site+"/_Incapsula_Resource?SWCGHOEL=gee")
     .set("cookie",cookies_str)
     .set("Content-Type", "application/x-www-form-urlencoded")
-    .set("accept-encoding", "gzip, deflate, br")
-    .set("user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36")
-    .set("referer",referer)
     .send(post_body);
-    console.log(r.text)
-    console.log(r)
-    console.log(post_body)
 
+    console.log(cookies_str)
     console.log("begin to get site page")
-    console.log(r.header)
-    r=await agent.get(site)
-    console.log(r.text)
-
+    r=await request.get(site).set("cookie",cookies_str)
+    console.log(r.text.length)
+*/
 
     return crack != null;
 }
