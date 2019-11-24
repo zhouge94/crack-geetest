7462c0cfd0e7a4a41f3219b373144fba157e30c2
zhouge <zhouge@aliyun>
2019-11-24 23:16:49 +0800

    www

--- www.js
+++ www.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 let crack_geetest = require('./');
 
-//添加的新用户数据
 var user = {
    "user4" : {
       "name" : "mohit",
@@ -12,7 +11,6 @@ var user = {
    }
 }
  async function testOnce(config) {
-    // B站登录
     console.log(config)
     let crack={}
     try {
@@ -24,26 +22,25 @@ var user = {
         console.log(crack);
     } catch (error) {
         console.log(String(error))
-        return {"sucess":0,"result":String(error)}
+        return {"success":0,"result":String(error)}
     }
         
-    if(!("geetest_challenge" in crack))return {"sucess":0,"result":""}
+    if(!crack)return {"success":0,"result":""}
     
     var post_body = "geetest_challenge=" + crack.challenge 
     + "&geetest_validate=" +  crack.validate 
     + "&geetest_seccode=" + crack.validate+"|jordan";
     console.log(post_body)
-    return {"sucess":1,"result":post_body}
+    return {"success":1,"result":post_body}
 }
 
-app.post('/addUser', async function (req, res) {
-   // 读取已存在的数据
-   console.log(req)
-   params=req.query
+app.get('/getval', async function (req, res) {
+   //console.log(req)
+   let params=req.query
    
-   r=await testOnce(params)
-   result=r["result"]
-   ret={"sucess":r["sucess"],"params":params,"result":result}
+   let r=await testOnce(params)
+   let result=r["result"]
+   let ret={"success":r["success"],"params":params,"result":result}
    res.end( JSON.stringify(ret));
 })
 
